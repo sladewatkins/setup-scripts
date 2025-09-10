56551ce7d69532b27d2ddd1f4a09a36ab10701c0
Slade Watkins <srw@sladewatkins.net>
2025-09-10 14:23:39 -0400

    win: add ``obs-multi-rtmp`` plugin

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,4 @@
 winget install AgileBits.1Password AgileBits.1Password.CLI Google.Chrome Valve.Steam Valve.SteamCMD EpicGames.EpicGamesLauncher GOG.Galaxy Roblox.Roblox Microsoft.Office Microsoft.PowerToys GitHub.GitHubDesktop Git.Git Notepad++.Notepad++ goatcorp.XIVLauncher Ookla.Speedtest.CLI Ookla.Speedtest.Desktop OBSProject.OBSStudio ChatterinoTeam.Chatterino ShareX.ShareX REALiX.HWiNFO --accept-package-agreements --accept-source-agreements
 Start-Process "https://cdn.cider.sh/winget/cider-v3.1.0-windows-x64.exe"
+Start-Process "https://github.com/sorayuki/obs-multi-rtmp/releases/download/0.7.3/obs-multi-rtmp-0.7.3.0-windows-x64-Installer.exe"
 irm https://raw.githubusercontent.com/emilwojcik93/Install-NvidiaApp/refs/heads/main/Install-NvidiaApp.ps1 | iex
