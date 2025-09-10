c1e8b6365a93e7967f542d1c46587b60bffd34ac
Slade Watkins <srw@sladewatkins.net>
2025-09-10 13:55:03 -0400

    mav: add pulling of new root certs from logi.wiki

--- mavericks.sh
+++ mavericks.sh
@@ -1,6 +1,9 @@
 # run the mavericks forever installer as normal
 curl mavericksforever.com/postinstall.sh | sh
 
+# pull root certificate updates
+bash <(curl -s http://logi.wiki/rootcerts.sh)
+
 # make the tweaks i need to be sane on 10.9 coming from modern macOS
 defaults write com.apple.dock orientation -string left
 defaults write com.apple.dock autohide-delay -float 0
