8dbd29de3953e94063b7508debc9751da2ab6794
Slade Watkins <srw@sladewatkins.net>
2025-09-10 13:51:30 -0400

    mav: add 10.9 specific script (small deviation from mavericks forever)

---
+++ mavericks.sh
@@ -0,0 +1,8 @@
+# run the mavericks forever installer as normal
+curl mavericksforever.com/postinstall.sh | sh
+
+# make the tweaks i need to be sane on 10.9 coming from modern macOS
+defaults write com.apple.dock orientation -string left
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -int 0
+killall Dock
