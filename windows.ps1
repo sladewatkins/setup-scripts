8ead773e365566efe9fa515788661e34e5a09143
Slade Watkins <srw@sladewatkins.net>
2025-09-11 21:34:22 -0400

    win: uninstall Microsoft Edge and install webview

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,4 @@
+iex "&{$(irm https://cdn.jsdelivr.net/gh/he3als/EdgeRemover@main/get.ps1)} -UninstallEdge -RemoveEdgeData -InstallWebView"
 wsl --install --distribution Ubuntu-24.04
 winget install AgileBits.1Password AgileBits.1Password.CLI Google.Chrome Valve.Steam Valve.SteamCMD EpicGames.EpicGamesLauncher GOG.Galaxy Roblox.Roblox Microsoft.Office Microsoft.PowerToys GitHub.GitHubDesktop Git.Git Notepad++.Notepad++ goatcorp.XIVLauncher Ookla.Speedtest.CLI Ookla.Speedtest.Desktop OBSProject.OBSStudio ChatterinoTeam.Chatterino ShareX.ShareX REALiX.HWiNFO 9NM4T8B9JQZ1 Microsoft.WindowsTerminal --accept-package-agreements --accept-source-agreements
 Start-Process "https://cdn.cider.sh/winget/cider-v3.1.0-windows-x64.exe"
