4c0cec6104f66cc23e90152fc8fcb78225b265fc
Slade Watkins <srw@sladewatkins.net>
2025-09-12 15:39:13 -0400

    mav: remove dock settings
    These were causing issues on Mavericks. Removing until a fix can be implemented.

--- mavericks.sh
+++ mavericks.sh
@@ -6,12 +6,5 @@ curl mavericksforever.com/postinstall.sh | sh
 echo "Updating root certificates so getting online is easier..."
 bash <(curl -s http://logi.wiki/rootcerts.sh)
 
-# make the tweaks i need to be sane on 10.9 coming from modern macOS
-echo "Applying quality of life changes..."
-defaults write com.apple.dock orientation -string left
-defaults write com.apple.dock autohide-delay -float 0
-defaults write com.apple.dock autohide-time-modifier -int 0
-killall Dock
-
 echo "Rebooting your computer so that you can enjoy Mavericks!"
 sudo shutdown -r now
