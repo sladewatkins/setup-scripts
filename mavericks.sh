c623d37c994d05de6f925a35687a2a665879fa4a
Slade Watkins <srw@sladewatkins.net>
2025-09-12 19:34:06 -0400

    mav: add xcode tools

--- mavericks.sh
+++ mavericks.sh
@@ -6,5 +6,10 @@ curl mavericksforever.com/postinstall.sh | sh
 echo "Updating root certificates so getting online is easier..."
 bash <(curl -s http://logi.wiki/rootcerts.sh)
 
+# Get Xcode Command Line Tools for Mavericks (and accept license terms)
+echo "Getting Xcode Command Line Tools and accepting license..."
+sudo xcode-select --install
+sudo xcodebuild -license
+
 echo "Rebooting your computer so that you can enjoy Mavericks!"
 sudo shutdown -r now
